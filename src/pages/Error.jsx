b59dfb9f69ba4fec6625d6e97d8e9e776113d60f
Stepan <nemail.necom@gmail.com>
2021-01-09 22:42:55 +0300

    Added Hero.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
 const Error = () => {
   return (
     <div>
-      Hello from error page
+     <Hero></Hero>
     </div>
   )
 }
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
 const Rooms = () => {
   return (
     <div>
-      Hello from rooms page
+      <Hero hero='roomsHero'></Hero>
     </div>
   )
 }
